086d33eb8eaee9083fa3c9f944d192a34b9d43f1
Andrey Bozhilov <andrei.bozhilov@gmail.com>
2015-12-20 19:39:07 +0200

    Fix masonry

--- Fridge/Fridge.WebClient/controllers/HomeController.js
+++ Fridge/Fridge.WebClient/controllers/HomeController.js
@@ -11,4 +11,25 @@
             { title: 'Overnight Blueberry French Toast', chef: 'KARAN1946', description: 'This is a very unique breakfast dish. Good for any holiday breakfast or brunch, it\'s filled with the fresh taste of blueberries, and covered with a rich blueberry sauce to make it a one of a kind.', image: 'http://images.media-allrecipes.com/userphotos/250x250/381517.jpg' },
             { title: 'Chef John\'s Perfect Prime Rib', chef: 'Chef John', description: 'this delicious chicken dish is exquisite and easy to prepare. the light and luscious lemon sauce really pops without being too acidic; it is simply divine. serve it with herb-roasted potatoes or lemon-rice pilaf.', image: 'http://images.media-allrecipes.com/userphotos/250x250/1876731.jpg' }
     ];
+
+    $scope.fixLayot = function () {
+        $(function () {
+            setTimeout(function () {
+                $(".grid").masonry();
+            }, 200);
+
+            $(window).resize(function () {
+                console.log("asd");
+                setTimeout(function () {
+                    $(".grid").masonry();
+                    console.log("asd");
+                }, 1000);
+
+
+            });
+
+        })
+    }
+
+    $scope.fixLayot();
 });

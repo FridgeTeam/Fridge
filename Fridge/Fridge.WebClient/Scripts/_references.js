04a2c2b5cf13a024078758cb61c999d544d51ded
ivzb <izahariev96@gmail.com>
2015-12-23 17:36:56 +0200

    recepie page implemented

--- Fridge/Fridge.WebClient/Scripts/_references.js
+++ Fridge/Fridge.WebClient/Scripts/_references.js
@@ -1,9 +1,18 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../app.js" />
+/// <reference path="../bower_components/angular/angular.js" />
+/// <reference path="../bower_components/angular-animate/angular-animate.js" />
+/// <reference path="../bower_components/angular-route/angular-route.js" />
+/// <reference path="../bower_components/angular-ui-bootstrap-bower/ui-bootstrap-tpls.js" />
+/// <reference path="../bower_components/bootstrap/dist/js/bootstrap.js" />
+/// <reference path="../bower_components/jquery/dist/jquery.js" />
+/// <reference path="../bower_components/noty/js/noty/packaged/jquery.noty.packaged.js" />
 /// <reference path="../controllers/homecontroller.js" />
 /// <reference path="../controllers/logincontroller.js" />
+/// <reference path="../controllers/recepiecontroller.js" />
 /// <reference path="../controllers/registercontroller.js" />
 /// <reference path="../controllers/user/UserSettingsController.js" />
+/// <reference path="../directives/recepie.js" />
 /// <reference path="../services/adminrequests.js" />
 /// <reference path="../services/baseurl.js" />
 /// <reference path="../services/notyservice.js" />
@@ -11,10 +20,6 @@
 /// <reference path="../services/requester.js" />
 /// <reference path="../services/userrequests.js" />
 /// <reference path="../services/usersession.js" />
-/// <reference path="E:\11.WepAPI\Fridge\Fridge\Fridge.WebClient\bower_components/angular/angular.js" />
-/// <reference path="E:\11.WepAPI\Fridge\Fridge\Fridge.WebClient\bower_components/jquery/dist/jquery.js" />
-/// <reference path="E:\11.WepAPI\Fridge\Fridge\Fridge.WebClient\bower_components/angular-ui-bootstrap-bower/ui-bootstrap.js" />
-/// <reference path="E:\11.WepAPI\Fridge\Fridge\Fridge.WebClient\bower_components/angular-ui-bootstrap-bower/ui-bootstrap-tpls.js" />
 
 
 

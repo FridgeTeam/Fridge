2e9df664be41f901efb83370d96ca1ff4474fc52
Andrey Bozhilov <andrei.bozhilov@gmail.com>
2015-12-24 17:34:35 +0200

    Missed file

---
+++ Fridge/Fridge.WebClient/directives/recipe.js
@@ -0,0 +1,9 @@
+﻿angular.module('app')
+.directive('ngRecipe', function () {
+    return {
+        scope: {
+            datasource: '='
+        },
+        templateUrl: 'directives/recipe.html'
+    };
+});
